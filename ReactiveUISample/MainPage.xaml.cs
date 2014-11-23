22bc491d4bc847d9268f15d2821bf356c497df26
maciej-progtech <maciejzbrzezny@gmail.com>
2014-11-23 10:22:53 +0100

    Sample what utilise Reactive UI

---
+++ ReactiveUISample/MainPage.xaml.cs
@@ -0,0 +1,68 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at http://go.microsoft.com/fwlink/?LinkId=234238
+
+namespace ReactiveUISample
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page, IViewFor<MyViewModel>
+    {
+        MyViewModel viewModel;
+        public MainPage()
+        {
+            this.InitializeComponent();
+            viewModel = new MyViewModel();
+            this.DataContext = ViewModel;
+
+            this.Bind(ViewModel, x => x.Name, x => x.name.Text);
+            this.WhenAny(x => x.ViewModel.FirstName, x => x.Value ?? "")
+               .BindTo(this, x => x.firstName.Text); 
+            this.WhenAny(x => x.ViewModel.LastName, x => x.Value ?? "")
+                .BindTo(this, x => x.lastName.Text);
+            //this.WhenAny(x => x.ViewModel.NameDays, x => x.Value ?? new List<string>() )
+            //    .BindTo(this, x => x.nameDays.ItemsSource);
+            //this.WhenAny(x => x.ViewModel.Temp, x => x.Value ?? new List<string>())
+            //    .BindTo(this, x => x.nameDays.ItemsSource);
+            //this.WhenAny(x => x.ViewModel.Temp, x => x.Value ?? new List<string>())
+            //    .BindTo(this, x => x.nameDays.ItemsSource);
+            //nameDays.ItemsSource = ViewModel.Temp;
+        }
+
+        //public MyViewModel ViewModel
+        //{
+        //    get;
+        //    set;
+        //    //get { return (MyViewModel)GetValue(ViewModelProperty); }
+        //    //set { SetValue(ViewModelProperty, value); }
+        //}
+        //public static readonly DependencyProperty ViewModelProperty =
+        //    DependencyProperty.Register("ViewModel", typeof(MyViewModel), typeof(MainPage), new PropertyMetadata(null));
+
+        public MyViewModel ViewModel
+        {
+            get { return viewModel; }
+            set { viewModel = value; }
+        }
+        object IViewFor.ViewModel
+        {
+            get { return viewModel; }
+            set { viewModel = (MyViewModel) value; }
+        }
+    }
+}
---
+++ ReactiveUISample/MyViewModel.cs
@@ -0,0 +1,121 @@
+﻿using ReactiveUI;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Reactive.Linq;
+using System.Threading;
+using System.Reactive.Concurrency;
+
+namespace ReactiveUISample
+{
+    public class MyViewModel : ReactiveObject
+    {
+        ObservableAsPropertyHelper<string> firstName;
+        ObservableAsPropertyHelper<string> lastName;
+        ObservableAsPropertyHelper<List<string>> nameDays;
+        string name = string.Empty;
+        public ReactiveCommand<object> ClearCommand { get; private set; }
+
+        public MyViewModel()
+        {
+            this.ThrownExceptions.Subscribe(x =>
+                {
+                    x.ToString();
+                });
+            firstName = this.WhenAny(x => x.Name, x => x.Value)
+                .Select(x => x.Split(' ')[0])
+                .ToProperty(this, x => x.FirstName);
+
+            lastName = this.WhenAny(x => x.Name, x => x.Value).Select(x =>
+            {
+                var s = x.Split(' ');
+                if (s.Count() > 1) return s[1]; else return string.Empty;
+            }).ToProperty(this, x => x.LastName);
+
+
+            // Transform the event stream into a stream of strings (the input values)
+            var input = this.WhenAnyValue(x => x.FirstName)
+                .Where(x => x == null || x.Length < 4)
+                .Throttle(TimeSpan.FromSeconds(3))
+                .Merge(this.WhenAnyValue(x => x.FirstName)
+                    .Where(x => x != null && x.Length >= 4)
+                    .Throttle(TimeSpan.FromMilliseconds(400)))
+                //.Select(args => SearchText)
+                //.Merge(
+                //    textBoxEnterCommand.Executed.Select(e => SearchText))
+                .DistinctUntilChanged();
+
+            // Setup an Observer for the search operation
+            var search = Observable.ToAsync<string, SearchResult>(DoSearch);
+
+            // Chain the input event stream and the search stream, cancelling searches when input is received
+            var results = from searchTerm in input
+                          from result in search(searchTerm).TakeUntil(input)
+                          select result.Results.Select(x => x.ToString()).ToList();
+
+            // Log the search result and add the results to the results collection
+            nameDays = results.ObserveOn(CoreDispatcherScheduler.Current)
+                .ToProperty(this, x => x.NameDays);
+
+            var firstNameOK=this.WhenAnyValue(x => x.FirstName)
+                .Select(x => !string.IsNullOrWhiteSpace(x) && x.Length >= 3);
+            var lastNameOK=this.WhenAnyValue(x => x.LastName)
+                .Select(x => !string.IsNullOrWhiteSpace(x) && x.Length >= 3);
+
+            ClearCommand = ReactiveCommand.Create(firstNameOK.Zip(lastNameOK,(x,y)=>x&&y));
+            ClearCommand.Subscribe(x => Name = string.Empty);
+        }
+
+        public string Name
+        {
+            get { return name; }
+            set { this.RaiseAndSetIfChanged(ref name, value); }
+        }
+        public string FirstName
+        {
+            get { return firstName.Value; }
+        }
+        public string LastName
+        {
+            get { return lastName.Value; }
+        }
+        public List<string> NameDays
+        {
+            get { return nameDays.Value; }
+        }
+
+        //public List<string> Temp
+        //{
+        //    get { return new List<string>{"aaa","bbb"}; }
+        //}
+
+        private readonly Random random = new Random(DateTime.Now.Millisecond);
+
+
+        private SearchResult DoSearch(string searchTerm)
+        {
+            ThreadSleep(random.Next(100, 500)); // Simulate latency
+            return new SearchResult
+            {
+                SearchTerm = searchTerm,
+                Results =
+                    NameDay.NameDays.Where(item => item.Names.ToUpperInvariant().Contains(searchTerm.ToUpperInvariant())).ToArray()
+            };
+        }
+        public struct SearchResult
+        {
+            public string SearchTerm { get; set; }
+            public IEnumerable<NameDay> Results { get; set; }
+
+        }
+        private void ThreadSleep(int milliseconds)
+        {
+            using (EventWaitHandle tmpEvent = new ManualResetEvent(false))
+            {
+                tmpEvent.WaitOne(TimeSpan.FromMilliseconds(milliseconds));
+            }
+        }
+    }
+}
